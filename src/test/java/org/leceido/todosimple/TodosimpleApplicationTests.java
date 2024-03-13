6f33f2584967717265e15838d0ad40f87693ee31
leceido <leceido@gmail.com>
2024-03-13 13:08:50 -0300

    Primeiro Commit

---
+++ src/test/java/org/leceido/todosimple/TodosimpleApplicationTests.java
@@ -0,0 +1,13 @@
+package org.leceido.todosimple;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodosimpleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
