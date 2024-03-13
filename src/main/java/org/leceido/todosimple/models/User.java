efe0c84cda9dd9aaabd4b353d69b582be4ee8389
leceido <leceido@gmail.com>
2024-03-13 20:01:19 -0300

    Model task

--- src/main/java/org/leceido/todosimple/models/User.java
+++ src/main/java/org/leceido/todosimple/models/User.java
@@ -8,11 +8,20 @@ import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 @Entity
 @Table(name = User.TABLE_NAME)
 public class User {
+    public List<Task> getTasks() {
+        return tasks;
+    }
+
+    public void setTasks(List<Task> tasks) {
+        this.tasks = tasks;
+    }
+
     public interface CreateUser {}
     public interface UpdateUser {}
     public static final String TABLE_NAME="user";
@@ -34,7 +43,8 @@ public class User {
     @Size(groups = {CreateUser.class, UpdateUser.class}, min = 8, max = 60)
     private String password;
 
-    //private List<Task> tasks = new ArrayList<Task>();
+    @OneToMany(mappedBy = "user")
+    private List<Task> tasks = new ArrayList<Task>();
 
 
     public User() {
