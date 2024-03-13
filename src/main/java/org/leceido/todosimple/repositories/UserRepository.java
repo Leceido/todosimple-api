e74f9ea680c5c52a06cb7249c47b4624bae39473
leceido <leceido@gmail.com>
2024-03-13 20:35:10 -0300

    criacao User e Task Repositories

---
+++ src/main/java/org/leceido/todosimple/repositories/UserRepository.java
@@ -0,0 +1,10 @@
+package org.leceido.todosimple.repositories;
+
+import org.leceido.todosimple.models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+}
